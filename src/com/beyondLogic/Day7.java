fd21e94e3664b40706ec88566e8a5cc16c977852
Beyond-Logic <edemaero@gmail.com>
2021-06-14 09:50:58 +0100

    30 Days to Code Challenge HackerRank
    //Day 7: Arrays

---
+++ src/com/beyondLogic/Day7.java
@@ -0,0 +1,31 @@
+package com.beyondLogic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Day7 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Collections.reverse(arr);
+
+        System.out.println(arr.toString()
+        .replace(",", "")
+        .replace("[", "")
+        .replace("]", ""));
+
+        bufferedReader.close();
+    }
+}
