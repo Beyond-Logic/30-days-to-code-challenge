3f00dfe24e7fca240bc5ba8602c67e51c77321eb
Beyond-Logic <edemaero@gmail.com>
2021-06-15 10:45:49 +0100

    30 Days to Code Challenge HackerRank
    //Day 8: Dictionaries and Maps - Challenge Completed

---
+++ src/com/beyondLogic/Day8.java
@@ -0,0 +1,30 @@
+package com.beyondLogic;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Day8 {
+
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        HashMap<String, Integer> phoneBook = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            phoneBook.put(name,phone);
+        }
+
+        while(in.hasNext()){
+            String s = in.next();
+            if(phoneBook.get(s) != null){
+                System.out.println(s + "=" + phoneBook.get(s));
+            }
+            else {
+                System.out.println("Not found");
+            }
+            // Write code here
+         }
+        in.close();
+    }
+
+}
